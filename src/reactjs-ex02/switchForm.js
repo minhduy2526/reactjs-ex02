3c55dbf8d6378b1aecdce847338fb8724b5f2951
Tu Minh Duy <duytm2599@gmail.com>
2022-05-25 23:47:29 +0700

    Second commit

--- src/reactjs-ex02/switchForm.js
+++ src/reactjs-ex02/switchForm.js
@@ -1,32 +1,45 @@
-import React, { Component } from 'react';
-import switchFormStyles from './switchForm.module.css';
-import model from '../assets/img/model.jpg';
-import glasses1 from '../assets/img/v1.png';
+import React, { Component } from "react";
+import switchFormStyles from "./switchForm.module.css";
+import model from "/public/assets/img/model.jpg";
+import glasses1 from "/public/assets/img/v1.png";
+import data from "../data/data.json";
 
 class SwitchForm extends Component {
-    render() {
-        return (
-            <div className={switchFormStyles.container}>
-                <div className={switchFormStyles.left}>
-                    <div className={switchFormStyles.imgModel} style={{
-                        backgroundImage: `url(${model})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                    }}>
-                        <img src={glasses1} alt='' />
-                        <div className={switchFormStyles.content}>
-                            <h1>GUCCI G8850U</h1>
-                            <p>Light pink square lenses define these sunglasses, ending with amother of pearl effect tip.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className={switchFormStyles.right}>
-                    <img src={model} alt=''/>
-                </div>
+  state = {
+    img: "",
+    name: "",
+    desc: "",
+  };
+
+  render() {
+    return (
+      <div className={switchFormStyles.container}>
+        <div className={switchFormStyles.left}>
+          <div
+            className={switchFormStyles.imgModel}
+            style={{
+              backgroundImage: "",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          >
+            <img src="" alt="" />
+            <div className={switchFormStyles.content}>
+              <h1>GUCCI G8850U</h1>
+              <p>
+                Light pink square lenses define these sunglasses, ending with
+                amother of pearl effect tip.
+              </p>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div className={switchFormStyles.right}>
+          <img src="" alt="" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SwitchForm;
